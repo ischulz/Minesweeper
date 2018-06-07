3de09e13959d1ce4c27ccd42e08e55f3f8770735
ischulz <iliasschulz@yahoo.com>
2018-06-07 16:47:07 -0700

    refactor cell component and include getcelltext func

--- minesweeper/src/Cell.jsx
+++ minesweeper/src/Cell.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import './Cell.css';
 
+const getCellText = (text, value, props) =>
+    props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === value && <div className="cellText">{text}</div>;
+  
+
 const Cell = ((props) => (
     <div 
       className={props.visualValue[props.row][props.cell] === 1 ? 'cell cellClicked' : 'cell cellUnclicked'}
       onClick={() => props.handleClick(props.row, props.cell)}
     >
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 66 && 'Bomb!'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 101 && '1'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 102 && '2'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 103 && '3'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 104 && '4'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 105 && '5'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 106 && '6'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 107 && '7'}
-      {props.visualValue[props.row][props.cell] === 1 && props.cellValue[props.row][props.cell] === 108 && '8'}
-
+      {getCellText('Bomb', 66, props) ||
+      getCellText(1, 101, props) ||
+      getCellText(2, 102, props) ||
+      getCellText(3, 103, props) ||
+      getCellText(4, 104, props) ||
+      getCellText(5, 105, props) ||
+      getCellText(6, 106, props) ||
+      getCellText(7, 107, props) ||
+      getCellText(8, 108, props)}
     </div>
 ));
 
